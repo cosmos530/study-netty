a3c8892e6ab489ae4c3fc775b4289491437309ad
cosmos530 <cosmos530@hanmail.net>
2017-04-19 00:00:03 +0900

    initial commit

---
+++ study-netty/src/main/java/com/zkwmajtm/StudyNettyApplication.java
@@ -0,0 +1,12 @@
+package com.zkwmajtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyNettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyNettyApplication.class, args);
+	}
+}
